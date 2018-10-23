caac1c9fe810e330c6be1a6b4de205df3f386ea3
Sophie Koonin <sophie.koonin@gmail.com>
2018-10-23 21:06:57 +0100

    WIP auth

---
+++ functions/attendance.js
@@ -0,0 +1,92 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const fetch = require('node-fetch');
+
+// eslint-disable-next-line
+Date.prototype.format = function() {
+  return `${this.getDate()}/${this.getMonth() + 1}/${this.getFullYear()}`;
+};
+
+exports.addAttendancePost = functions
+  .region('europe-west1')
+  .https.onRequest((req, res) => {
+    const attendancePostContent =
+      ':dancing_banana: Rehearsal day! :dancing_banana: <!channel> \n' +
+      'Please indicate whether or not you can attend tonight by reacting to this message with :thumbsup:' +
+      '(present) or :thumbsdown: (absent).\n' +
+      'Facilitator please respond with :raised_hands:!\n' +
+      'To volunteer for Physical warm up, respond with :muscle: ' +
+      'For Musical warm up, respond with :musical_note:.';
+
+    fetch('https://slack.com/api/chat.postMessage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        Authorization: `Bearer ${functions.config().shebot.token}`
+      },
+      body: JSON.stringify({
+        text: attendancePostContent,
+        channel: 'C2Z635VUJ',
+        icon_emoji: ':ballot_box_with_check:',
+        as_user: false,
+        username: 'Attendance Bot',
+        attachments: [
+          {
+            callback_id: 'attendance_reply',
+            actions: [
+              {
+                name: 'attending',
+                text: ':+1:',
+                type: 'button',
+                value: 'pressed'
+              },
+              {
+                name: 'notAttending',
+                text: ':-1:',
+                type: 'button',
+                value: 'pressed'
+              },
+              {
+                name: 'physical',
+                text: ':muscle:',
+                type: 'button',
+                value: 'pressed'
+              },
+              {
+                name: 'musical',
+                text: ':musical_note:',
+                type: 'button',
+                value: 'pressed'
+              },
+              {
+                name: 'facilitator',
+                text: ':raised_hands:',
+                type: 'button',
+                value: 'pressed'
+              }
+            ]
+          }
+        ]
+      })
+    })
+      .then(res => res.json())
+      .then(({ message }) => {
+        const { ts, channel } = message;
+        return admin
+          .firestore()
+          .collection('attendance')
+          .add({
+            rehearsal_date: new Date().format(),
+            ts: ts,
+            attending: [],
+            notAttending: [],
+            notResponded: []
+          });
+      })
+      .then(result =>
+        res.send(`Message posted! Saved to datastore with ID ${result.id}.`)
+      )
+      .catch(err => {
+        throw new Error(err);
+      });
+  });
---
+++ functions/auth.js
@@ -0,0 +1,69 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+
+// With thanks to Dennis Alund https://medium.com/evenbit/building-a-slack-app-with-firebase-as-a-backend-151c1c98641d
+
+exports.oauth_redirect = functions
+  .region('europe-west1')
+  .https.onRequest((request, response) => {
+    if (request.method !== 'GET') {
+      console.error(`Got unsupported ${request.method} request. Expected GET.`);
+      return response.send(405, 'Only GET requests are accepted');
+    }
+
+    if (!request.query && !request.query.code) {
+      return response.status(401).send("Missing query attribute 'code'");
+    }
+
+    const queryParams = [
+      `code=${request.query.code}`,
+      `client_id=${functions.config().shebot.id}`,
+      `client_secret=${functions.config().shebot.secret}`,
+      `redirect_uri=https://europe-west1-${
+        process.env.GCLOUD_PROJECT
+      }.cloudfunctions.net/oauth_redirect`
+    ];
+    const encodedQueryString = querystring.stringify(queryParams);
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf8'
+      }
+    };
+
+    const result = fetch(
+      `https://slack.com/api/oauth.access?${encodedQueryString}`,
+      options
+    )
+      .then(result => result.json())
+      .then(result => {
+        if (!result.ok) {
+          console.error('The request was not ok: ' + JSON.stringify(result));
+          return response
+            .header(
+              'Location',
+              `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com`
+            )
+            .send(302);
+        }
+
+        return null;
+        //return  admin
+        //   .firestore()
+        //   .collection('installations')
+        //   .add({
+        //     token: result.access_token,
+        //     team: result.team_id
+        //   })
+      })
+      .then(() =>
+        response
+          .header(
+            'Location',
+            `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/success.html`
+          )
+          .send(302)
+      );
+  });
