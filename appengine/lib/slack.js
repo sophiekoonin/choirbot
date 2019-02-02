6444d48682891bd638636e49e9615d34b9e9c5f3
Sophie Koonin <sophie.koonin@gmail.com>
2019-02-02 15:27:51 +0000

    fix test endpoint

--- appengine/lib/slack.js
+++ appengine/lib/slack.js
@@ -185,6 +185,12 @@ exports.postRehearsalMusic = async function(req, res) {
 
 exports.testSlackIntegration = async function(req, res) {
   try {
+    const [team_id, channel_id] = await utils.getDbOrConfigValues(
+      'config',
+      'slack',
+      ['team_id', 'channel_id']
+    );
+    const token = await getToken(team_id);
     await slack.chat.postMessage({
       token,
       text: 'Test post, please ignore!',
