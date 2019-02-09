70e51710e33e5c17e7b7fcac896fa216ef732a19
Sophie Koonin <sophie.koonin@gmail.com>
2019-02-09 16:37:28 +0000

    Keep trying to fix auth

--- appengine/lib/google.js
+++ appengine/lib/google.js
@@ -5,35 +5,18 @@ const db = require('./db');
 
 const { NODE_ENV } = process.env;
 const sheets = google.sheets('v4');
-
-const { auth, sheetId } = init().then(res => res);
-
-async function init() {
-  try {
-    const authClient = await google.auth.getClient({
-      scopes: ['https://www.googleapis.com/auth/spreadsheets']
-    });
-    const id = await utils.getDbOrConfigValue('config', 'google', 'sheet_id');
-    return { authClient, id };
-  } catch (err) {
-    console.error('Error initialising Google APIs:', err);
-    throw err;
-  }
-}
-
 function getValuesAndFlatten(response) {
   const { values } = response.data;
   return [].concat.apply([], values);
 }
 
-async function getRowNumberForDate(dateString) {
+async function getRowNumberForDate(auth, sheetId, dateString) {
   const request = {
     auth,
     spreadsheetId: sheetId,
     range: 'A:A'
   };
   try {
-    console.log(auth);
     const response = await sheets.spreadsheets.values.get(request);
     const rowNumber = getValuesAndFlatten(response).indexOf(dateString) + 1;
     return rowNumber > 0 ? rowNumber : 1;
@@ -43,7 +26,7 @@ async function getRowNumberForDate(dateString) {
   }
 }
 
-async function getSongDetailsFromSheet(rowNumber) {
+async function getSongDetailsFromSheet(auth, sheetId, rowNumber) {
   try {
     const response = await sheets.spreadsheets.values.get({
       auth,
@@ -76,15 +59,35 @@ async function getSongDetailsFromSheet(rowNumber) {
 
 exports.getNextSongs = async function(dateString) {
   try {
-    const rowNumber = await getRowNumberForDate(dateString);
-    return await getSongDetailsFromSheet(rowNumber);
+    const auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    });
+    const sheetId = await utils.getDbOrConfigValue(
+      'config',
+      'google',
+      'sheet_id'
+    );
+    const rowNumber = await getRowNumberForDate(auth, sheetId, dateString);
+    return await getSongDetailsFromSheet(auth, sheetId, rowNumber);
   } catch (err) {
     throw err;
   }
 };
 
 exports.testGoogleIntegration = async function(req, res) {
-  const testDate = '04/02/2019';
-  const rowNumber = await getRowNumberForDate(testDate);
-  res.status(200).send(rowNumber);
+  try {
+    const auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    });
+    const sheetId = await utils.getDbOrConfigValue(
+      'config',
+      'google',
+      'sheet_id'
+    );
+    const testDate = '04/02/2019';
+    const rowNumber = await getRowNumberForDate(auth, sheetId, testDate);
+    res.status(200).send(`Row number is ${rowNumber}`);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
