021ab3c1cc972d9bcc131c95199f4f96344ed273
Sophie Koonin <10195023+sophiekoonin@users.noreply.github.com>
2019-02-25 17:20:39 +0000

    Fix attendance post message

--- appengine/lib/utils.js
+++ appengine/lib/utils.js
@@ -67,8 +67,9 @@ exports.getAttendancePostMessage = function({
     ':dancing_banana: Rehearsal day! :dancing_banana: <!channel> \n' +
     `*Today's rehearsal:* ${mainSong}\n` +
     ` ${runThrough && `*Run through:* ${runThrough}\n\n`}` +
-    ` ${notes.toLowerCase().includes('team updates') &&
-      '*Team updates meeting at 6:30! All welcome* :tada:\n'}` +
+    ${notes.toLowerCase().includes('team updates')
+      ? 'Team updates meeting at 6:30! All welcome :tada:\n'
+      : ''} +
     'Please indicate whether or not you can attend tonight by reacting to this message with :thumbsup: ' +
     '(present) or :thumbsdown: (absent).\n' +
     'Facilitator please respond with :raised_hands:!\n' +
