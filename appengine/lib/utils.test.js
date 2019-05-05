d98523491d80ce0743d8df54f8ce23b2ccebbd33
Sophie Koonin <sophie.koonin@gmail.com>
2019-05-05 16:42:36 +0100

    Add test for rehearsal music message

--- appengine/lib/utils.test.js
+++ appengine/lib/utils.test.js
@@ -1,4 +1,7 @@
-const { getAttendancePostMessage } = require('./utils');
+const {
+  getAttendancePostMessage,
+  getRehearsalMusicMessage
+} = require('./utils');
 
 describe('utils', () => {
   describe('getAttendancePostMessage', () => {
@@ -21,6 +24,60 @@ describe('utils', () => {
       const mainSong = 'Bills Bills Bills';
       const notes = '';
       const result = getAttendancePostMessage({ mainSong, notes });
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  describe('getRehearsalMusicMessage', () => {
+    it('should format correctly with all bits present', () => {
+      const args = {
+        mainSong: 'Hide and Seek',
+        mainSongLink: 'link-to-hideandseek',
+        runThrough: 'Dreams',
+        runThroughLink: 'link-to-dreams',
+        notes: 'Something something team updates 6:30'
+      };
+      const result = getRehearsalMusicMessage(args);
+      expect(result).toMatchSnapshot();
+    });
+    it('should format correctly with no main song link', () => {
+      const args = {
+        mainSong: 'Hide and Seek',
+        runThrough: 'Dreams',
+        runThroughLink: 'link-to-dreams',
+        notes: 'Something something team updates 6:30'
+      };
+      const result = getRehearsalMusicMessage(args);
+      expect(result).toMatchSnapshot();
+    });
+    it('should format correctly with no run through', () => {
+      const args = {
+        mainSong: 'Hide and Seek',
+        mainSongLink: 'link-to-hideandseek',
+        notes: 'Something something team updates 6:30'
+      };
+      const result = getRehearsalMusicMessage(args);
+      expect(result).toMatchSnapshot();
+    });
+    it('should format correctly with run through but no link', () => {
+      const args = {
+        mainSong: 'Hide and Seek',
+        mainSongLink: 'link-to-hideandseek',
+        runThrough: 'Dreams',
+        notes: 'Something something team updates 6:30'
+      };
+      const result = getRehearsalMusicMessage(args);
+      expect(result).toMatchSnapshot();
+    });
+    it('should format correctly with no team updates', () => {
+      const args = {
+        mainSong: 'Hide and Seek',
+        mainSongLink: 'link-to-hideandseek',
+        runThrough: 'Dreams',
+        runThroughLink: 'link-to-dreams',
+        notes: ''
+      };
+      const result = getRehearsalMusicMessage(args);
       expect(result).toMatchSnapshot();
     });
   });
