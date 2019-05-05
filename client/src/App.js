199bded4dd128013200d21d3e1ee5b6e7725a7c9
Sophie Koonin <sophie.koonin@gmail.com>
2019-05-05 17:28:30 +0100

    separate into client and server

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Report from './Report/Report';
+
+function App() {
+  return (
+    <div className="App">
+      <Report />
+    </div>
+  );
+}
+
+export default App;
