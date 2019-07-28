aacb4aae81c7cac8c9046f0548638ab0a092c77f
Sophie Koonin <sophie.koonin@gmail.com>
2019-07-28 10:51:29 +0100

    Set google sheets ID via slash command:

--- app/src/slack/slash-commands.js
+++ app/src/slack/slash-commands.js
@@ -1,19 +1,23 @@
 const slack = require('slack');
 const db = require('../db');
 
-// exports.handleSlashCommands = async (req, res) => {
-//   const { text } = req.body;
-//   const textAsArray = text.split()[0];
-//   const command = textAsArray[0];
-//   switch (command) {
-//     case 'sheet':
-//       const sheetId = textAsArray[1];
+exports.handleSlashCommands = async (req, res) => {
+  const { text, team_id } = req.body;
+  const textAsArray = text.split(' ');
+  const command = textAsArray[0];
+  switch (command) {
+    case 'sheet':
+      setGoogleSheetId(team_id, textAsArray[1]);
+      return res.send(
+        `I've set your Google Sheets ID to \`${
+          textAsArray[1]
+        }\` - if that's not right, you can do this again to reset it.`
+      );
+    default:
+      return res.send("Sorry, I didn't understand that!");
+  }
+};
 
-//     default:
-//       return res.send("Sorry, I didn't understand that!");
-//   }
-// };
-
-// async function setGoogleSheetId(teamId, sheetId) {
-
-// }
+async function setGoogleSheetId(teamId, sheetId) {
+  await db.updateDbValue('teams', teamId, { google_sheet_id: sheetId });
+}
