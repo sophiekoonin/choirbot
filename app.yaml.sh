b86589775b3e7e9fc13e808eaa81157ac5fbf391
Sophie Koonin <sophie.koonin@gmail.com>
2019-11-25 19:44:40 +0000

    Add circle ci config

---
+++ app.yaml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo """
+runtime: nodejs12
+instance_class: F1
+automatic_scaling:
+  max_instances: 1
+env_variables:
+  GCLOUD_PROJECT: $GCLOUD_PROJECT
+  SLACK_CLIENT_ID: $SLACK_CLIENT_ID
+  SLACK_CLIENT_SECRET: $SLACK_CLIENT_SECRET
+  SLACK_SIGNING_SECRET: $SLACK_SIGNING_SECRET
+"""
