0514ab3fa856cd28c7771f7c72c73c90ed8c6114
Sophie Koonin <sophie@localghost.dev>
2024-05-12 16:30:25 +0100

    don't need to use fake timers

--- jest.setup.js
+++
@@ -1,3 +0,0 @@
-// https://stackoverflow.com/questions/50793885/referenceerror-you-are-trying-to-import-a-file-after-the-jest-environment-has
-//github.com/nock/nock/issues/2200
-https: jest.useFakeTimers({ doNotFake: ['nextTick', 'setImmediate'] })
